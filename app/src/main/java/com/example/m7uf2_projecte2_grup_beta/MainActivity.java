50d31a8433156465c00f07b1937e104d75f11797
joan_ <jroselliborra@gmail.com>
2021-03-08 22:26:35 +0100

    -Afegit comentari de prova.

--- app/src/main/java/com/example/m7uf2_projecte2_grup_beta/MainActivity.java
+++ app/src/main/java/com/example/m7uf2_projecte2_grup_beta/MainActivity.java
@@ -8,6 +8,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Debug;
 import android.view.View;
 
 import android.view.Menu;
@@ -22,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab;
+        fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
