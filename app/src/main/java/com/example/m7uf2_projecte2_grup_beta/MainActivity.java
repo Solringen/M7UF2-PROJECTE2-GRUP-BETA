75ebd9644d4ec827a7e8cc6d8ce05a9d08a50a55
joan_ <jroselliborra@gmail.com>
2021-03-08 22:28:21 +0100

    -Afegit comentari de prova.

--- app/src/main/java/com/example/m7uf2_projecte2_grup_beta/MainActivity.java
+++ app/src/main/java/com/example/m7uf2_projecte2_grup_beta/MainActivity.java
@@ -16,6 +16,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    //AAAAA
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
