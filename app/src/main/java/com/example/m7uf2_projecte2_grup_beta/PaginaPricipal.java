4da4228ba302f36f726f0158e57eb8fc2c0c7762
ivanf <ivanfloresin99@gmail.com>
2021-03-18 14:00:26 +0100

    PaginaPricipal

--- app/src/main/java/com/example/m7uf2_projecte2_grup_beta/PaginaPricipal.java
+++ app/src/main/java/com/example/m7uf2_projecte2_grup_beta/PaginaPricipal.java
@@ -1,7 +1,10 @@
 package com.example.m7uf2_projecte2_grup_beta;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class PaginaPricipal extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class PaginaPricipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagina_pricipal);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(PaginaPricipal.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
+
     }
 }
