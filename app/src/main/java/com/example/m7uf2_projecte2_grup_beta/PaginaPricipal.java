fe82fa0b589dce88e927a249d1fde16078d30725
ivanf <ivanfloresin99@gmail.com>
2021-03-18 09:53:09 +0100

    Prova

---
+++ app/src/main/java/com/example/m7uf2_projecte2_grup_beta/PaginaPricipal.java
@@ -0,0 +1,13 @@
+package com.example.m7uf2_projecte2_grup_beta;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaginaPricipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagina_pricipal);
+    }
+}
