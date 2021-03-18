95b09f7eb05f0c8572d947566e30a36d08566e9b
ivanf <ivanfloresin99@gmail.com>
2021-03-18 08:58:00 +0100

    Prova03

--- app/src/main/java/com/example/m7uf2_projecte2_grup_beta/MainActivity.java
+++ app/src/main/java/com/example/m7uf2_projecte2_grup_beta/MainActivity.java
@@ -1,18 +1,8 @@
 package com.example.m7uf2_projecte2_grup_beta;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.os.Debug;
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
