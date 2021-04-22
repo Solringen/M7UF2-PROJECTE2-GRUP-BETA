61563683bea382d8201f05abebbcaa3382c49d09
ivanf <ivanfloresin99@gmail.com>
2021-04-22 09:30:20 +0200

    ecultures

---
+++ app/src/main/java/com/example/m7uf2_projecte2_grup_beta/EsculturasAdapter.java
@@ -0,0 +1,67 @@
+package com.example.m7uf2_projecte2_grup_beta;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+
+public class EsculturasAdapterFirestore extends FirestoreRecyclerAdapter<Esculturas, EsculturasAdapterFirestore.EsculturasHolder> {
+
+    // Classe "holder" que proporciona una referència a als components de cada element
+    // de la llista. La declarem com a classe interna per millorar l'encapsulació de l'Adapter.
+    public class EsculturasHolder extends RecyclerView.ViewHolder {
+        public View element;
+        public TextView tv01, tv02;
+        public ImageView image01,image02;
+
+
+        public EsculturasHolder(View itemView) {
+            super(itemView);
+            tv01 = (TextView) itemView.findViewById(R.id.tv01);
+            image01 = (ImageView) itemView.findViewById(R.id.image01);
+
+            tv02 = (TextView) itemView.findViewById(R.id.tv02);
+            image02 = (ImageView) itemView.findViewById(R.id.image02);
+        }
+    }
+
+    // Constructor de l'Adapter que reaprofita directament el constructor de la classe pare
+    // que inicialitza l'Adapter amb l'objecte "Options" que ens passen (l'objecte options es
+    // crea dins el mètode onCreate() de l'activity que conté el RecyclerView.
+    public EsculturasAdapterFirestore(@NonNull FirestoreRecyclerOptions<Esculturas> opcions) {
+        super(opcions);
+    }
+
+    // Mètode que "lliga" els atributs dels objecte Alumne amb cadascun dels components gràfics
+    // on els volem visualitzar dins de cada element del RecyclerView
+    @Override
+    protected void onBindViewHolder(@NonNull EsculturasHolder holder, int position, @NonNull Esculturas esc) {
+        holder.tv01.setText(esc.getTitol());
+        holder.tv02.setText(esc.getTitol());
+
+        Bitmap bmp = BitmapFactory.decodeByteArray(esc.getImatge().toBytes(), 0, esc.getImatge().toBytes().length);
+        holder.image01.setImageBitmap(Bitmap.createScaledBitmap(bmp, holder.image01.getWidth(), holder.image01.getHeight(), false));
+
+        Bitmap bmp01 = BitmapFactory.decodeByteArray(esc.getImatge().toBytes(), 0, esc.getImatge().toBytes().length);
+        holder.image02.setImageBitmap(Bitmap.createScaledBitmap(bmp01, holder.image02.getWidth(), holder.image02.getHeight(), false));
+    }
+
+    // Mètode que fa l'inflate (conversió de XML a objectes) de cada element del RecyclerView a partir
+    // del disseny en XML que tenim a res/layout/alumne.xml.
+    @NonNull
+    @Override
+    public EsculturasHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View vista = LayoutInflater.from(parent.getContext()).inflate(R.layout.vista4_estructura, parent, false);
+        return new EsculturasHolder(vista);
+    }
+}
